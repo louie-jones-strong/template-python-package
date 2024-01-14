edff6fa5dbfb8028c013b08df169666af3d02bea
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2024-01-14 15:50:19 +0000

    add badges

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # template-python-package
 Template repository for storing, testing, linting, building and releasing a python package
+
+
+# YOUR PACKAGE NAME
+<!-- badges -->
+![Tests](./media/badges/tests.svg)
+![Coverage](./media/badges/coverage.svg)
+
+<!-- brief description -->
+*Brief description of your package*
--- tests/Test_Example.py
+++ tests/Test_Example.py
@@ -1,8 +1,19 @@
-import unittest
+import pytest
 
 
-class Test_Agents(unittest.TestCase):
+class Test_Example:
 
     def test_1(self):
-        self.assertEqual(1, 1)
+        assert 1 == 1
+        return
+
+    def test_2(self):
+        assert 1 == 2
+        return
+
+    # skip this test
+
+    @pytest.mark.skip(reason="no way of currently testing this")
+    def test_3(self):
+        assert 1 == 2
         return
